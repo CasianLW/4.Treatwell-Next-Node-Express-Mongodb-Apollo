94ae40470be011cf6a77b46dc7c6f419c25c8462
CasianLW <ciorbcasian@gmail.com>
2024-03-08 01:23:31 +0100

    test main index

---
+++ treatwell-frontend/pages/index.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+// import Layout from "./layout";
+import BookingComponent from "@/components/BookingComponent";
+
+export default function Home() {
+  return (
+    <div>
+      <div>Content here</div>
+      <BookingComponent />
+    </div>
+    // </Layout>
+  );
+}

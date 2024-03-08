409e0a89d673e0e43e32c7a282bd9e3cb372a5bd
CasianLW <ciorbcasian@gmail.com>
2024-03-08 01:20:27 +0100

    updated user schema with multiple days and infos for user/ cancellation

--- treatwell-backend/models/Coiffeur.js
+++ treatwell-backend/models/Coiffeur.js
@@ -1,20 +1,46 @@
 const mongoose = require("mongoose");
 
-const daySchema = new mongoose.Schema({
-  date: Date, // Represents a specific day
-  slots: [
-    {
-      type: Number, // Represents a specific 15-minute slot within the day
-      default: [], // By default, all slots are available (empty array means all slots are open)
+const slotSchema = new mongoose.Schema({
+  slotNumber: {
+    type: Number,
+    required: true,
+  },
+  name: String,
+  phoneNumber: String,
+  email: String,
+  reservationId: {
+    type: String,
+    default: function () {
+      // This function generates a random string for the reservation ID
+      // You can replace this logic with any ID generation logic you prefer
+      return Math.random().toString(36).substr(2, 9);
     },
-  ],
+    unique: true, // Make sure the reservation ID is unique across all documents
+  },
+});
+
+// const daySchema = new mongoose.Schema({
+//   date: Date, // Represents a specific day
+//   slots: [
+//     {
+//       type: Number, // Represents a specific 15-minute slot within the day
+//       default: [], // By default, all slots are available (empty array means all slots are open)
+//     },
+//   ],
+// });
+const daySchema = new mongoose.Schema({
+  date: {
+    type: Date,
+    required: true,
+  },
+  slots: [slotSchema],
 });
 
 const CoiffeurSchema = new mongoose.Schema({
   nom: String,
   prenom: String,
   urlImage: String,
-  joursTravail: [daySchema], // An array of 'day' objects
+  joursTravail: [daySchema],
 });
 
 module.exports = mongoose.model("Coiffeur", CoiffeurSchema);

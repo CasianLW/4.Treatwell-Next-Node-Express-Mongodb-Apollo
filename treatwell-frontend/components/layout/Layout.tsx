34f48fc38235cad24781877c3e508a41218514f7
CasianLW <ciorbacasian@gmail.com>
2024-03-11 17:50:51 +0100

    added error context for debugging

--- treatwell-frontend/components/layout/Layout.tsx
+++ treatwell-frontend/components/layout/Layout.tsx
@@ -1,5 +1,8 @@
-import { FC, ReactNode } from "react";
+import { FC, ReactNode, useEffect } from "react";
 import Head from "next/head";
+import ErrorDisplay from "../ErrorDisplay";
+import { ErrorProvider, useError } from "@/contexts/ErrorContext";
+import { setHandleApolloError } from "@/lib/apolloClient";
 
 type LayoutProps = {
   children: ReactNode;
@@ -14,26 +17,32 @@ const Layout: FC<LayoutProps> = ({
 }) => {
   return (
     <>
-      <Head>
-        <title>{title}</title>
-        <meta name="description" content={description} />
-        <link rel="icon" href="/favicon.ico" />
-        {/* Add any other head elements here */}
-      </Head>
-      <nav>
-        <ul>
-          <li>
-            <a href="/#reservations">Reservations</a>
-          </li>
-          <li>
-            <a href="/#about">About</a>
-          </li>
-          <li>
-            <a href="/#contact">Contact</a>
-          </li>
-        </ul>
-      </nav>
-      <main>{children}</main>
+      <ErrorProvider>
+        <Head>
+          <title>{title}</title>
+          <meta name="description" content={description} />
+          <link rel="icon" href="/favicon.ico" />
+          {/* Add any other head elements here */}
+        </Head>
+        <nav>
+          <ul>
+            <li>
+              <a href="/#reservations">Reservations</a>
+            </li>
+            <li>
+              <a href="/#about">About</a>
+            </li>
+            <li>
+              <a href="/#contact">Contact</a>
+            </li>
+          </ul>
+        </nav>
+        <main>
+          <ErrorDisplay />
+
+          {children}
+        </main>
+      </ErrorProvider>
     </>
   );
 };

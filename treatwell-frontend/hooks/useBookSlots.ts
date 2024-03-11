6ae35611cac9e29d23e9bde9e0b595705f13e1b7
CasianLW <ciorbcasian@gmail.com>
2024-03-11 17:53:05 +0100

    book slot hook

--- treatwell-frontend/hooks/useBookSlots.ts
+++ treatwell-frontend/hooks/useBookSlots.ts
@@ -1,4 +1,6 @@
 import { gql, useMutation } from "@apollo/client";
+import { GET_COIFFEURS } from "./useCoiffeurUpdatedSubscription";
+import ICoiffeur from "@/types/Coiffeur";
 
 const BOOK_SLOTS_MUTATION = gql`
   mutation BookSlots($coiffeurId: ID!, $date: String!, $slots: [SlotInput!]!) {
@@ -19,7 +21,40 @@ const BOOK_SLOTS_MUTATION = gql`
 `;
 
 export const useBookSlots = () => {
-  const [bookSlots, { data, loading, error }] =
-    useMutation(BOOK_SLOTS_MUTATION);
+  // const [bookSlots, { data, loading, error }] =
+  //   useMutation(BOOK_SLOTS_MUTATION);
+  // return { bookSlots, data, loading, error };
+
+  const [bookSlots, { data, loading, error }] = useMutation(
+    BOOK_SLOTS_MUTATION,
+    {
+      update(cache, { data: { bookSlots } }) {
+        // Attempt to read the query from the cache
+        const cacheData = cache.readQuery<{ coiffeurs: ICoiffeur[] }>({
+          query: GET_COIFFEURS,
+        });
+
+        // Ensure we have data to work with
+        if (cacheData) {
+          const { coiffeurs } = cacheData;
+
+          // Find and update the coiffeur data with the new slots
+          const updatedCoiffeurs = coiffeurs.map((coiffeur) => {
+            if (coiffeur.id === bookSlots.id) {
+              return { ...coiffeur, joursTravail: bookSlots.joursTravail };
+            }
+            return coiffeur;
+          });
+
+          // Write the updated coiffeurs list back to the cache
+          cache.writeQuery({
+            query: GET_COIFFEURS,
+            data: { coiffeurs: updatedCoiffeurs },
+          });
+        }
+      },
+    }
+  );
+
   return { bookSlots, data, loading, error };
 };

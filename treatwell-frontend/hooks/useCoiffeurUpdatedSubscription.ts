af548c863c1006cdecc3bfc74595ba24e4ba14aa
CasianLW <ciorbacasian@gmail.com>
2024-03-11 17:53:26 +0100

    updated gql data

--- treatwell-frontend/hooks/useCoiffeurUpdatedSubscription.ts
+++ treatwell-frontend/hooks/useCoiffeurUpdatedSubscription.ts
@@ -1,7 +1,27 @@
 import { gql, useSubscription } from "@apollo/client";
 import ICoiffeur from "@/types/Coiffeur";
 
-const COIFFEUR_UPDATED_SUBSCRIPTION = gql`
+export const GET_COIFFEURS = gql`
+  query {
+    coiffeurs {
+      id
+      nom
+      prenom
+      joursTravail {
+        date
+        slots {
+          slotNumber
+          name
+          phoneNumber
+          email
+          reservationId
+        }
+      }
+    }
+  }
+`;
+
+export const COIFFEUR_UPDATED_SUBSCRIPTION = gql`
   subscription OnCoiffeurUpdated {
     coiffeurUpdated {
       coiffeurId
@@ -33,12 +53,26 @@ interface CoiffeurUpdatePayload {
   };
 }
 
+// export const useCoiffeurUpdatedSubscription = (
+//   onUpdate: (payload: CoiffeurUpdatePayload) => void
+// ) => {
+//   useSubscription<CoiffeurUpdatePayload>(COIFFEUR_UPDATED_SUBSCRIPTION, {
+//     onSubscriptionData: ({ subscriptionData }) => {
+//       if (subscriptionData && subscriptionData.data) {
+//         onUpdate(subscriptionData.data);
+//       }
+//     },
+//   });
+// };
+
 export const useCoiffeurUpdatedSubscription = (
   onUpdate: (payload: CoiffeurUpdatePayload) => void
 ) => {
   useSubscription<CoiffeurUpdatePayload>(COIFFEUR_UPDATED_SUBSCRIPTION, {
     onSubscriptionData: ({ subscriptionData }) => {
+      // console.log("Subscription data received:", subscriptionData);
       if (subscriptionData && subscriptionData.data) {
+        // console.log("Updating client with new data...");
         onUpdate(subscriptionData.data);
       }
     },

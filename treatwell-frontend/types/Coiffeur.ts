721754939d635d871491f25241e08fa766295fa8
CasianLW <ciorbacasian@gmail.com>
2024-03-11 17:49:24 +0100

    updated schema to correct matching one

--- treatwell-frontend/types/Coiffeur.ts
+++ treatwell-frontend/types/Coiffeur.ts
@@ -1,17 +1,18 @@
-interface ISlot {
+export interface ISlot {
   slotNumber: number;
   name?: string;
   phoneNumber?: string;
   email?: string;
-  reservationId: string;
+  reservationId?: string;
 }
 
-interface IDay {
-  date: Date;
+export interface IDay {
+  date: Date | number | string;
   slots: ISlot[];
 }
 
 interface ICoiffeur extends Document {
+  id: string;
   nom: string;
   prenom: string;
   urlImage: string;

7043d2be55186fac796a4d6faed869ee703e4cb4
CasianLW <ciorbcasian@gmail.com>
2024-03-19 09:53:53 +0100

    multi langage base config

--- treatwell-frontend/next.config.mjs
+++ treatwell-frontend/next.config.mjs
@@ -1,5 +1,12 @@
-/** @type {import('next').NextConfig} */
+// /** @type {import('next').NextConfig} */
+// const { i18n } = require("./next-i18next.config");
+// import { i18n } from "./next-i18next.config";
+
 const nextConfig = {
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en", "fr", "es"],
+  },
   images: {
     domains: ["images.unsplash.com"], // Add the Unsplash hostname here
   },

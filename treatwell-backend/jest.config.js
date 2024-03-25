b4bfdd757005641b7810869469a756ccd8b76821
CasianLW <ciorbcasian@gmail.com>
2024-03-25 23:01:54 +0100

    jest setup link

--- treatwell-backend/jest.config.js
+++ treatwell-backend/jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-  preset: "ts-jest",
+  //   preset: "@shelf/jest-mongodb",
   testEnvironment: "node",
   testMatch: ["**/__tests__/**/*.js?(x)", "**/?(*.)+(spec|test).js?(x)"],
+  setupFiles: ["<rootDir>/jest.setup.js"],
   //   testMatch: ["**/__tests__/**/*.ts?(x)", "**/?(*.)+(spec|test).ts?(x)"],
 };

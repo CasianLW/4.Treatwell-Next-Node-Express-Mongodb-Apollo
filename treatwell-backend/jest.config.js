df654f635bfee2eed8680d80a70d5baa58095ca8
CasianLW <ciorbcasian@gmail.com>
2024-03-25 18:35:02 +0100

    ts -> js for tests

--- treatwell-backend/jest.config.js
+++ treatwell-backend/jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
-  testMatch: ["**/__tests__/**/*.ts?(x)", "**/?(*.)+(spec|test).ts?(x)"],
+  testMatch: ["**/__tests__/**/*.js?(x)", "**/?(*.)+(spec|test).js?(x)"],
+  //   testMatch: ["**/__tests__/**/*.ts?(x)", "**/?(*.)+(spec|test).ts?(x)"],
 };
